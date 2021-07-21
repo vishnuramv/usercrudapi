7c553b29ddf47aaa98e55082d151808fcf4070ab
vishnuramv <vishnu.v1902@gmail.com>
2021-07-21 12:41:59 +0530

    Fix: content type2

--- user_crud/settings.py
+++ user_crud/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import django_heroku 
-
+import os
 env = environ.Env()
 
 environ.Env.read_env()
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT ='static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'/static/'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
