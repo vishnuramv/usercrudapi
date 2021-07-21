b04e18d6a662685e9c11eb8344a2678c7fbb35f2
vishnuramv <vishnu.v1902@gmail.com>
2021-07-21 11:49:48 +0530

    Add: heroku configs

--- user_crud/settings.py
+++ user_crud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku 
 
 env = environ.Env()
 
@@ -136,3 +137,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
