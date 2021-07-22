6d1f9ad46ab7d7150c86d07be845f39ae3075f2e
vishnuramv <vishnu.v1902@gmail.com>
2021-07-22 20:35:54 +0530

    Fix: corsheader

--- user_crud/settings.py
+++ user_crud/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'usercrud',
     'rest_framework',
     'coreapi',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,4 +149,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
 django_heroku.settings(locals())
